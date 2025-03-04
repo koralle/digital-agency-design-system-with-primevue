32b27016fe50746a119cccda12c630d2986d44e1
koralle <mkhd7930@gmail.com>
2025-03-04 19:58:56 +0900

    feat: run format

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,23 +1,20 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {
-      'builder': {
-        viteConfigPath: 'vite.config.ts'
-      }
-    }
-  }
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'vite.config.ts',
+      },
+    },
+  },
 };
 export default config;
--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,12 +1,12 @@
-import type { Preview } from '@storybook/vue3'
-import './styles.css'
+import type { Preview } from '@storybook/vue3';
+import './styles.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
