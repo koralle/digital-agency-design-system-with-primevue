819ce2953a71f8785ff37759ac0c64523d7e40e4
koralle <mkhd7930@gmail.com>
2025-03-04 15:49:33 +0900

    feat: add `.prettierrc.mjs`

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
