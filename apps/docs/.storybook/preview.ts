2a840566cb69a4ad15fbf349799126f082a4801f
koralle <mkhd7930@gmail.com>
2025-03-04 20:28:50 +0900

    feat(docs): introduce PrimeVue to Storybook

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,5 +1,13 @@
 import type { Preview } from '@storybook/vue3';
+import { setup } from '@storybook/vue3';
 import './styles.css';
+import PrimeVue from 'primevue/config';
+
+setup((app) => {
+  app.use(PrimeVue, {
+    unstyled: true
+  })
+})
 
 const preview: Preview = {
   parameters: {
